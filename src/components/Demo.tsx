85739823cf3e0ae14d4d18d3585f4989c04c59b0
Joseph McCullough <joseph@vertstudios.com>
2024-06-12 09:46:13 -0500

    Update Demo

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,10 +1,10 @@
 import { PropsWithChildren, useState } from 'react'
 import { TrackableButton } from '../lib/TrackableButton'
 
-type PinkButtonProps = PropsWithChildren & {
+type MyButtonProps = PropsWithChildren & {
     [x:string]: any;
 }
-const MyButton = ({ children, ...rest }: PinkButtonProps) => {
+const MyButton = ({ children, ...rest }: MyButtonProps) => {
     return <button style={{ borderRadius: '6px' }} {...rest}>{children}</button>
 }
 
@@ -18,7 +18,7 @@ export const Demo = () => {
         <>
             <div className="flex-container">
                 <div className="flex-item"><p>Clicks: {clicks}</p></div>
-                <div className="flex-item"><p><TrackableButton as={MyButton} eventName="pink_trackable_button_clicked" onClick={onClick}>Click me!</TrackableButton></p></div>
+                <div className="flex-item"><p><TrackableButton as={MyButton} eventName="trackable_button_clicked" onClick={onClick}>Click me!</TrackableButton></p></div>
             </div>
         </>
     )
